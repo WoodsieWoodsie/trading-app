7526363446830c5b2e1838f45351621e0a939308
Ammar <ammar@ammaronline.net>
2015-11-17 22:25:37 -0800

    whatever

--- models/user.js
+++ models/user.js
@@ -9,8 +9,8 @@ var User;
 
 var userSchema = Schema({
   username: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
   password: { type: String, required: true },
+  name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
 });
--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,18 @@
 $(document).ready(init);
 
 function init() {
-  
-
-
+  $('.register').click(register);
 }
+
+function register() {
+  var user = {
+    username: $().val(),
+    password1: $().val(),
+    password2: $().val(),
+    name: $().val(),
+    email: $().val(),
+    phone: $().val()
+  }
+  $.post()
+}
+
